9c7851be49c5fc5b87c7f83345b4b10ea196e482
shsheeila <74360604+shsheeila@users.noreply.github.com>
2020-12-13 23:20:12 +0800

    Update koneksi.php

--- inc/koneksi.php
+++ inc/koneksi.php
@@ -1,3 +1,8 @@
 <?php
-$koneksi = new mysqli ("localhost","root","","db_vote");
+//$koneksi = new mysqli ("localhost","root","","db_vote");
+$host = 'remotemysql.com';
+$db = 'gcWB8qzyAy';
+$user = 'gcWB8qzyAy';
+$pass = 'qjnZnDy9xW';
+$charset = 'utf8mb4';
 ?>
